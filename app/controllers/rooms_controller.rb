21ef845b1c9efd7ffd9ed2f24e687ae54f32123a
Alexandre Courivaud <alexandre.courivaud@gmail.com>
2023-05-05 17:40:42 +0200

    maj de l'ajout de peinture

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,30 +9,30 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @item = Item.new
+    @painting = Painting.new
   end
 
   def new
     @room = Room.new
-    @painting = room.paintings.build
+    @room.paintings.build
   end
 
   def create
     @room = Room.new(room_params)
-
     if @room.save
-      redirect_to rooms_path, notice: "La pièce #{@room.name} a bien été ajoutée."
+      redirect_to rooms_path, notice: "La pièce #{room.name} a bien été ajoutée."
     else
-      redirect_to rooms_path, notice: 'Ne peut être vide'
+      redirect_to rooms_path, notice: "Ne peut être vide"
     end
   end
 
-
   def edit
+    @room = Room.find(params[:id])
   end
 
   def update
     if @room.update(room_params)
-      redirect_to rooms_path, notice: "La pièce #{@room.name} a été mise à jour."
+      redirect_to rooms_path, notice: "La pièce #{room.name} a été mise à jour."
     else
       render :edit
     end
@@ -42,15 +42,16 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     @room.items.destroy_all
     @room.delete
-    redirect_to rooms_path, notice: "La pièce #{@room.name} a bien été supprimée."
+    redirect_to rooms_path, notice: "La pièce #{room.name} a bien été supprimée."
   end
 
   private
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    def room_params
-      params.require(:room).permit(:name, paintings_attributes: [:color, :brand, :date_painted])
-    end
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, paintings_attributes: [:id, :color, :brand, :date_painted])
+  end
 end
